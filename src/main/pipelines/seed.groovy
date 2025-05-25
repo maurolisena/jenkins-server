6de6a15b14a4035e96cc029195c42cf0b29fefe8
Mauro Lisena <maurolisena@gmail.com>
2025-05-25 18:51:05 -0300

    Migrating and configuring keycloack-server jenkinsfile

---
+++ src/main/pipelines/seed.groovy
@@ -0,0 +1,27 @@
+pipelineJob('config-server') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maurolisena/jenkins-server.git') }
+                    branch('main')
+                }
+            }
+            scriptPath('src/main/pipelines/config-server/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('keycloack-server') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maurolisena/jenkins-server.git') }
+                    branch('main')
+                }
+            }
+            scriptPath('src/main/pipelines/keycloack-server/Jenkinsfile')
+        }
+    }
+}
