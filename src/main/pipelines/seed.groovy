7b1ca4684cf1b0c37d82a167de97e76700fbfd70
Mauro Lisena <maurolisena@gmail.com>
2025-05-25 19:59:51 -0300

    Updating jenkins seed-job

--- src/main/pipelines/seed.groovy
+++ src/main/pipelines/seed.groovy
@@ -32,10 +32,66 @@ pipelineJob('gateway-server') {
             scm {
                 git {
                     remote { url('https://github.com/maurolisena/jenkins-server.git') }
-                    branch('master')
+                    branch('main')
                 }
             }
             scriptPath('src/main/pipelines/api-gateway/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('kafka-server') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maurolisena/jenkins-server.git') }
+                    branch('main')
+                }
+            }
+            scriptPath('src/main/pipelines/kafka-server/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('eureka-server') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maurolisena/jenkins-server.git') }
+                    branch('main')
+                }
+            }
+            scriptPath('src/main/pipelines/eureka-server/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('product-service') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maurolisena/jenkins-server.git') }
+                    branch('main')
+                }
+            }
+            scriptPath('src/main/pipelines/product-service/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('inventory-service') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maurolisena/jenkins-server.git') }
+                    branch('main')
+                }
+            }
+            scriptPath('src/main/pipelines/inventory-service/Jenkinsfile')
         }
     }
 }
